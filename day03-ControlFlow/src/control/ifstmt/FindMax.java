743d601aa4d0c29c20e933b8b6e014bee67e5bc1
z1w0n <zizcy@gmail.com>
2019-07-08 15:30:52 +0900

    제어흐름
    if 구문들

---
+++ day03-ControlFlow/src/control/ifstmt/FindMax.java
@@ -0,0 +1,52 @@
+package control.ifstmt;
+
+import java.util.Scanner;
+
+public class FindMax {
+
+	public static void main(String[] args) {
+		// 1. 선언
+		int x;
+		int y;
+		int max = 0;
+		Scanner scan;
+		
+		// 2. 초기화
+		scan = new Scanner(System.in);
+
+		 System.out.println("두 정수를 입력 (space bar 로 분리");  
+		 
+		 x = scan.nextInt();
+		 y = scan.nextInt();
+		 
+		 // 3. 사용
+		 
+		
+		 if (x > y) {
+		     max = x;
+		     
+		 }
+		 
+		 if (y > x) {
+		     max = y;
+		 }
+		     
+		 if (x == y) {
+		     max =x;
+		 }  
+		 System.out.printf("입력된 두 값 x=%d, y=%d 중 큰 값은 %d%n" , x, y, max);
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	}
+
+}
---
+++ day03-ControlFlow/src/control/ifstmt/MultipleOfTwo.java
@@ -0,0 +1,10 @@
+package control.ifstmt;
+
+public class MultipleOfTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
