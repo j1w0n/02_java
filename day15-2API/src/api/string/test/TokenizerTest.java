f15a797a9bdaf2f8ffad01d03057272f6d8ca2e9
z1w0n <zizcy@gmail.com>
2019-07-26 10:29:42 +0900

    String Tokenizer

---
+++ day15-2API/src/api/string/test/TokenizerTest.java
@@ -0,0 +1,20 @@
+package api.string.test;
+
+/**
+ * StringTokernizer 클래스를 테스트
+ * 
+ * StringTokernizer 는 java.util 패키지에 있으므로
+ * java.lang 에 있는 Object, String, StringBuilder와 같이
+ * 기본으로 사용할 수 없고 반드시 명시적으로 import 구문이 들어가야 사용가능
+ * @author Administrator
+ *
+ */
+public class TokenizerTest {
+
+	public static void main(String[] args) {
+		// 1. 선언
+		StringTokernizer token;
+		
+	}
+
+}
